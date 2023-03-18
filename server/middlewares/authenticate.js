796c3bef9e7292e003e44c750765e9ed47b7ef49
Muhammad Hamza <m.waqas.hamza@gmail.com>
2023-03-18 20:13:50 +0500

    handle missing cookie

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -2,6 +2,12 @@ const { getUserByToken } = require('../models/user');
 
 const authenticate = (req, res, next) => {
   const { token } = req.cookies;
+
+  if (!token) {
+    res.status(401).end();
+    return;
+  }
+
   getUserByToken(token)
     .then((userObj) => {
       req.user = userObj;
