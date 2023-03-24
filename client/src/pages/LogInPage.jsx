6fa99fcb7c76bf365c1f4c86aff2c907997229ad
MohammadMaazOwais <116021113+MohammadMaazOwais@users.noreply.github.com>
2023-03-25 02:33:20 +0500

    Updated Log In (#7)
    * Updated Log In
    
    Sign Up page yet to be made but log in is done. Sign up is simpler so easier to do. Plus need to clarify a few things first
    
    * fix linting errors
    
    ---------
    
    Co-authored-by: Muhammad Hamza <m.waqas.hamza@gmail.com>

--- client/src/pages/LogInPage.jsx
+++ client/src/pages/LogInPage.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useContext } from 'react';
+import { Button, Grid, TextField } from '@mui/material';
+import styled from '@emotion/styled';
 import { UserContext } from '../context/UserContext';
 
 function LogInPage() {
@@ -16,6 +18,7 @@ function LogInPage() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(email, password);
     logIn(email, password)
       .then(() => {
         window.location.reload();
@@ -26,26 +29,182 @@ function LogInPage() {
   };
 
   return (
-    <form>
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        name="email"
-        value={email}
-        onChange={handleEmailChange}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        value={password}
-        onChange={handlePasswordChange}
-      />
-      <button type="submit" onClick={handleSubmit}>
-        Log In
-      </button>
-    </form>
+    <Grid
+      container
+      direction="column"
+      bgcolor="white"
+      width={700}
+      height={500}
+      borderRadius="20px"
+      alignContent
+      padding={5}
+    >
+      <Grid
+        container
+        direction="column"
+        bgcolor="white"
+        width={200}
+        height={500}
+        borderRadius="20px"
+        alignContent
+        padding={5}
+      >
+        <Triangle />
+        <Square />
+        <Circle />
+      </Grid>
+      <Grid
+        container
+        direction="column"
+        bgcolor="#E9D6EC"
+        width={400}
+        height={500}
+        borderRadius="20px"
+        alignContent
+        padding={5}
+      >
+        <Grid item xl={3} md={3} sm={3} xs={3}>
+          <br />
+          <Title> Welcome Back!</Title>
+          <Subtitle fontFamily="Inter">
+            Welcome Back. Please enter your details
+          </Subtitle>
+        </Grid>
+        <Grid item xl={3} md={3} sm={3} xs={3} marginBottom={-4}>
+          {/* <Grid item xl={3} md={3} sm={3} xs={3} alignContent={true}>
+
+        </Grid> */}
+          <Grid
+            item
+            xl={3}
+            md={3}
+            sm={3}
+            xs={3}
+            paddingLeft={8.3}
+            fontSize={11}
+            color="#786E6E"
+            fontWeight={500}
+          >
+            Email:
+          </Grid>
+          <TextField
+            style={{
+              backgroundColor: 'white',
+              borderRadius: '4px',
+            }}
+            InputProps={{
+              style: {
+                width: '190px',
+                height: '25px',
+              },
+            }}
+            className="inputbox"
+            id="outlined-input"
+            name="email"
+            onChange={handleEmailChange}
+          />
+        </Grid>
+        <Grid item xl={3} md={3} sm={3} xs={3}>
+          <Grid
+            item
+            xl={3}
+            md={3}
+            sm={3}
+            xs={3}
+            paddingLeft={8.3}
+            fontSize={11}
+            color="#786E6E"
+            fontWeight={500}
+          >
+            Password:
+          </Grid>
+          <TextField
+            style={{
+              backgroundColor: 'white',
+              borderRadius: '4px',
+            }}
+            InputProps={{
+              style: {
+                width: '190px',
+                height: '25px',
+              },
+            }}
+            InputLabelProps={
+              {
+                // shrink: false,
+              }
+            }
+            id="password"
+            name="password"
+            onChange={handlePasswordChange}
+          />
+        </Grid>
+        <Grid item xl={3} md={3} sm={3} xs={3}>
+          <Button
+            variant="outlined"
+            sx={{
+              color: 'white',
+              backgroundColor: '#0b0b45',
+              borderColor: '#0b0b45',
+            }}
+            onClick={handleSubmit}
+            style={{
+              height: 25,
+              width: 190,
+              fontSize: 9,
+              paddingTop: 6,
+              fontWeight: 400,
+              textTransform: 'none',
+            }}
+          >
+            Log In
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
+
+const Title = styled.div`
+  font-weight: 800;
+  font-family: Inter;
+`;
+
+const Subtitle = styled.div`
+  font-size: 6.9px;
+  font-family: Inter;
+  font-weight: 500;
+`;
+
+const Square = styled.div`
+  background-color: #96bdc6;
+  height: 60px;
+  width: 60px;
+  position: relative;
+  top: 50px;
+  right: -50px;
+`;
+
+const Circle = styled.div`
+  background-color: #0b0b45;
+  height: 60px;
+  width: 60px;
+  position: relative;
+  top: 30px;
+  right: -25px;
+  border-radius: 30px;
+`;
+
+const Triangle = styled.div`
+  background-color: white;
+  width: 0;
+  height: 0;
+  border-left: 30px solid transparent;
+  border-right: 30px solid transparent;
+  border-bottom: 60px solid #e9d6ec;
+  position: relative;
+  top: 110px;
+  right: 20px;
+`;
 
 export default LogInPage;
