b497c5a67feaf7ded3c71e6cc9b274c69c9ac841
Muhammad Hamza <m.waqas.hamza@gmail.com>
2023-03-28 04:31:24 +0500

    add router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 import App from './App';
 import UserContextProvider from './context/UserContext';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <App />
-    </UserContextProvider>
+    <RouterProvider router={router}>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </RouterProvider>
   </React.StrictMode>
 );
