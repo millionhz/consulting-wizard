3bf9b005dffb4e3181add2a2db7db7330c15c076
Muhammad Hamza <m.waqas.hamza@gmail.com>
2023-03-29 12:01:41 +0500

    implement RoleAwareComponent

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,95 @@
 import { useContext } from 'react';
-import { RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+  Navigate,
+  Outlet,
+} from 'react-router-dom';
 import { UserContext } from './context/UserContext';
+import LogInPage from './pages/LogInPage';
+import ManageProfilePage from './pages/ManageProfilePage';
+import userTypes from './utils/userTypes';
+
+function LoggedInInterceptor({ redirect }) {
+  const { isAuthenticated } = useContext(UserContext);
+
+  if (isAuthenticated === false) {
+    return <Navigate to={redirect} />;
+  }
+
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
+
+  return <div>Loading...</div>;
+}
+
+function LoggedOutInterceptor({ redirect }) {
+  const { isAuthenticated } = useContext(UserContext);
+
+  if (isAuthenticated === false) {
+    return <Outlet />;
+  }
+
+  if (isAuthenticated) {
+    return <Navigate to={redirect} />;
+  }
+
+  return <div>Loading...</div>;
+}
+
+function RoleAwareComponent({ client, consultant, admin }) {
+  const { type } = useContext(UserContext);
+
+  if (type === userTypes.CLIENT) {
+    return client;
+  }
+
+  if (type === userTypes.CONSULTANT) {
+    return consultant;
+  }
+
+  if (type === userTypes.ADMIN) {
+    return admin;
+  }
+}
 
 function App() {
-  const { router } = useContext(UserContext);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route element={<LoggedInInterceptor redirect="/login" />}>
+          <Route
+            index
+            element={
+              <RoleAwareComponent
+                client={<h1>Client</h1>}
+                consultant={<h1>Consultant</h1>}
+                admin={<h1>Admin</h1>}
+              />
+            }
+          />
+          <Route
+            path="manage-profile"
+            element={
+              <RoleAwareComponent
+                client={<ManageProfilePage />}
+                consultant={<ManageProfilePage />}
+                admin={<Navigate to="/" />}
+              />
+            }
+          />
+          <Route path="dashboard" element={<h1>dashboard</h1>} />
+          <Route path="about" element={<h1>about</h1>} />
+        </Route>
+        <Route element={<LoggedOutInterceptor redirect="/" />}>
+          <Route path="login" element={<LogInPage />} />
+        </Route>
+      </Route>
+    )
+  );
 
   return <RouterProvider router={router} />;
 }
--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,11 +1,5 @@
 import { createContext, useEffect, useMemo, useState } from 'react';
-import { authenticate } from '../api/backend';
-import BaseRouter from '../router/BaseRouter';
-import LoggedOutRouter from '../router/LoggedOutRouter';
-import ClientRouter from '../router/ClientRouter';
-import ConsultantRouter from '../router/ConsultantRouter';
-import AdminRouter from '../router/AdminRouter';
-import userTypes from '../utils/userTypes';
+import { authenticate as getAuthStatus } from '../api/backend';
 
 export const UserContext = createContext();
 
@@ -13,43 +7,21 @@ function UserContextProvider({ children }) {
   const [userObj, setUserObj] = useState({
     isAuthenticated: null,
   });
-  const [router, setRouter] = useState(BaseRouter);
 
-  useEffect(() => {
-    authenticate()
+  const authenticate = () =>
+    getAuthStatus()
       .then(({ data }) => {
         setUserObj({ ...data, isAuthenticated: true });
       })
       .catch(() => {
         setUserObj({ isAuthenticated: false });
       });
-  }, []);
 
   useEffect(() => {
-    const { isAuthenticated, type } = userObj;
+    authenticate();
+  }, []);
 
-    if (isAuthenticated === null) {
-      return setRouter(BaseRouter);
-    }
-
-    if (isAuthenticated === false) {
-      return setRouter(LoggedOutRouter);
-    }
-
-    if (type === userTypes.CLIENT) {
-      return setRouter(ClientRouter);
-    }
-
-    if (type === userTypes.CONSULTANT) {
-      return setRouter(ConsultantRouter);
-    }
-
-    if (type === userTypes.ADMIN) {
-      return setRouter(AdminRouter);
-    }
-  }, [userObj]);
-
-  const value = useMemo(() => ({ ...userObj, router }), [userObj, router]);
+  const value = useMemo(() => ({ ...userObj, authenticate }), [userObj]);
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 }
