9ad79c85149a097f8375e64e30b062eb52632a74
umamanasir <77013222+umamanasir@users.noreply.github.com>
2023-04-09 07:56:12 +0500

    ..

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import UserContextProvider from './context/UserContext';
-import Landing from './pages/client/LandingPage';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserContextProvider>
-      <Landing />
+      <App />
     </UserContextProvider>
   </React.StrictMode>
 );
