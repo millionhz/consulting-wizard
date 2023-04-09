863d2ee3b270dfcc9f0667365e8bc121c1c7a567
Muhammad Hamza <m.waqas.hamza@gmail.com>
2023-04-10 01:37:52 +0500

    change styled to emotion

--- client/src/pages/consultant/SetTimeSlots.jsx
+++ client/src/pages/consultant/SetTimeSlots.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 import 'react-calendar/dist/Calendar.css';
 import Calendar from '../../components/Calendar';
 import TimePickerModal from '../../components/TimePickerModal';
