d97059279df2f2d8977caa6ed6b17266ee66ad18
Muhammad Hamza <m.waqas.hamza@gmail.com>
2023-04-14 14:42:38 +0500

    Timezone patch (#55)
    * remove dateTime
    
    * add dateTime logic to frontEnd

--- client/src/utils/dateTime.js
+++ client/src/utils/dateTime.js
@@ -1,17 +1,12 @@
-const getDate = (dateTime) => {
+export const getDate = (dateTime) => {
   const date = new Date(dateTime);
   date.setHours(0, 0, 0, 0);
   return date;
 };
 
-const getNextDate = (dateTime) => {
+export const getNextDate = (dateTime) => {
   const date = new Date(dateTime);
   date.setDate(date.getDate() + 1);
   date.setHours(0, 0, 0, 0);
   return date;
-};
-
-module.exports = {
-  getNextDate,
-  getDate,
 };
