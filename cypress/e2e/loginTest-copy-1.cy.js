f040c95346d01820427e538fe784b26a3e209af6
Muhammad Ahmed <24100265@lums.edu.pk>
2023-04-27 17:23:31 +0500

    Login Tests

--- cypress/e2e/loginTest-copy-1.cy.js
+++ cypress/e2e/loginTest-copy-1.cy.js
@@ -1,27 +1,24 @@
-describe('Login Test', () => {
-  it('should log in successfully', () => {
-    cy.visit('https://consulting-wizard-production.up.railway.app/login') // replace with the URL of your login page
-    cy.get('input[name="email"]').type('hamza@app.com') // replace with your email input field name
-    cy.get('input[name="password"]').type('123456') // replace with your password input field name
-    cy.get('button[type="button"]').click() // replace with your login button selector
+describe("Login Test", () => {
+  it("should log in successfully", () => {
+    cy.visit("https://consulting-wizard-production.up.railway.app/login");
+    cy.get('input[name="email"]').type("hamza@app.com");
+    cy.get('input[name="password"]').type("123456");
+    cy.get('button[type="button"]').click();
 
-    cy.url().should('include', '/') // replace with the URL of your dashboard page
-  })
+    cy.url().should("include", "/");
+  });
 
-  it('should show error message for incorrect password', () => {
-    cy.visit('https://consulting-wizard-production.up.railway.app/login')
-    cy.get('input[name="email"]').type('admin@app.com')
-    cy.get('input[name="password"]').type('12345')
-    cy.get('button[type="button"]').click()
-    
-  })
+  it("should show error message for incorrect password", () => {
+    cy.visit("https://consulting-wizard-production.up.railway.app/login");
+    cy.get('input[name="email"]').type("admin@app.com");
+    cy.get('input[name="password"]').type("12345");
+    cy.get('button[type="button"]').click();
+  });
 
-
-  it('should show error message for incorrect email', () => {
-    cy.visit('https://consulting-wizard-production.up.railway.app/login')
-    cy.get('input[name="email"]').type('admin@123.com')
-    cy.get('input[name="password"]').type('123456')
-    cy.get('button[type="button"]').click()
-  })
-
-})
+  it("should show error message for incorrect email", () => {
+    cy.visit("https://consulting-wizard-production.up.railway.app/login");
+    cy.get('input[name="email"]').type("admin@123.com");
+    cy.get('input[name="password"]').type("123456");
+    cy.get('button[type="button"]').click();
+  });
+});
